c4a927072e0bbd026317f6d165c25d2f644ec0e9
Pruthvi P <pruthvi@ixigo.com>
2018-12-09 01:14:15 +0530

    new Build

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -17,7 +17,7 @@ module.exports = options => ({
   output: Object.assign(
     {
       // Compile into js/build.js
-      path: path.resolve(process.cwd(), 'build'),
+      path: path.resolve(process.cwd(), 'doc'),
       publicPath: '/',
     },
     options.output,
